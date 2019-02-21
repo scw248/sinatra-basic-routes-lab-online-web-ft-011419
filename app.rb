e595e6ae1e8fa81d2b1a101c369bdf4f24a25871
Stephen Wright <scw248@gmail.com>
2019-02-21 19:51:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do
-    "Hello, World!"
+  get '/name' do
+    "My name is _"
+  end
+  
+  get '/hometown' do
+    "My hometown is _"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is _"
   end
 end
