32907018997d39d7b309f39198a6aa7a20c3ef7c
Stephen Wright <scw248@gmail.com>
2019-02-21 19:51:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,14 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do
-    "My name is _"
+    "My name is Stephen"
   end
   
   get '/hometown' do
-    "My hometown is _"
+    "My hometown is Cincinnati"
   end
   
   get '/favorite-song' do
-    "My favorite song is _"
+    "My favorite song is 'Night Moves"
   end
 end
